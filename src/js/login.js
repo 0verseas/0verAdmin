f8ab329048d3b0aacaa4947c0f868aa7fa3ea450
richegg <zxp86021@gmail.com>
2018-07-06 15:23:49 +0800

    login 可以動了 XD

---
+++ src/js/login.js
@@ -0,0 +1,71 @@
+var login = (function () {
+
+    /**
+     * cache DOM
+     */
+
+    var $username = $('#username');
+    var $password = $('#password');
+    var $loginBtn = $('#btn-login');
+    var $errMsg = $('#errMsg');
+    var $getStatus = $('#getStatus');
+
+    /**
+     * bind event
+     */
+
+    $loginBtn.on('click', _login);
+    $password.on('keydown', _login);
+    $getStatus.on('click', _getStatus);
+
+    // 登入：
+    // 200: 跳轉至 /school
+    // 401: 顯示錯誤訊息
+    function _login(e) {
+        if (e.type == 'keydown' && e.keyCode != 13) {
+            return;
+        }
+
+        var username = $username.val();
+        var password = $password.val();
+
+        if (!username || !password) {
+            return;
+        }
+
+        var loginForm = {
+            username: username,
+            password: sha256(password)
+        };
+
+        User.login(loginForm).then(function(res) {
+            if(res.ok) {
+                return res.json();
+            } else {
+                throw res.status;
+            }
+        }).then(function(json) {
+            console.log(json);
+            //window.location.href = '/school/systemQuota.html'
+        }).catch(function(err) {
+            if (err == 401) {
+                $errMsg.finish().show().text('帳號密碼錯誤。').fadeOut(1500);
+            }
+        })
+    }
+
+    function _getStatus() {
+        User.isLogin().then(function(res) {
+            if(res.ok) {
+                return res.json();
+            } else {
+                throw res
+            }
+        }).then(function(json) {
+            console.log(json);
+        }).catch(function(err) {
+            console.log(err);
+        })
+    }
+
+})();
---
+++ src/js/user.js
@@ -0,0 +1,86 @@
+var User = (function () {
+    var _userInfo;
+    var baseUrl = env.baseUrl;
+
+    function _setUserInfo(userInfo) {
+        _userInfo = userInfo;
+    }
+
+    function login(loginForm) {
+        return fetch(baseUrl + '/admins/login', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify(loginForm)
+        });
+    }
+
+    function logout() {
+        return fetch(baseUrl + '/admins/logout', {
+            method: "POST",
+            credentials: 'include'
+        });
+    }
+
+    function isLogin() {
+        return fetch(baseUrl + '/admins/login', {
+            credentials: 'include'
+        });
+    }
+
+    // permission: 'admin'
+    function checkLogin(permission) {
+        return isLogin().then(function (res) {
+            if(res.ok) {
+                return res.json();
+            } else {
+                throw res.status;
+            }
+        }).then(function (json) {
+            if (!json[permission] || json[permission].has_banned) {
+                location.replace('/login.html');
+            } else {
+                _setUserInfo(json);
+            }
+        }).catch(function (err) {
+            if (err == 401) {
+                location.replace('/login.html');
+            }
+        });
+    }
+
+    function update(userInfo) {
+        return fetch(baseUrl + `/schools/me/editors/${userInfo.username}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify(userInfo)
+        }).then(function (res) {
+            if(res.ok) {
+                return res.json();
+            } else {
+                throw res.status;
+            }
+        }).then(function (json) {
+            _setUserInfo(json);
+            Sidebar.showUserInfo();
+        });
+    }
+
+    function getUserInfo() {
+        return _userInfo
+    }
+
+    return {
+        login,
+        logout,
+        isLogin,
+        checkLogin,
+        getUserInfo,
+        update
+    }
+})();
