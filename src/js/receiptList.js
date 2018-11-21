008981fdb01521f05a847e6ddb28f9f87009a6f6
He-Wen Chen <lotus3698@gmail.com>
2018-11-21 17:01:07 +0800

    收件清冊頁面

---
+++ src/js/receiptList.js
@@ -0,0 +1,23 @@
+
+
+
+$('#mc_selection').attr('href', env.baseUrl + '/admins/verified/mc-selection');
+$('#mc_placement').attr('href', env.baseUrl + '/admins/verified/mc-placement');
+$('#mc_foreign_edu').attr('href', env.baseUrl + '/admins/verified/mc-foreign-edu');
+
+$('#test_area_selection').attr('href', env.baseUrl + '/admins/verified/test-area-selection');
+$('#myanmar_teacher_education').attr('href', env.baseUrl + '/admins/verified/myanmar-teacher-education');
+$('#myanmar_placement').attr('href', env.baseUrl + '/admins/verified/myanmar-placement');
+$('#test_area_placement').attr('href', env.baseUrl + '/admins/verified/test-area-placement');
+$('#mya_sg_ph_to_preparatory_program').attr('href', env.baseUrl + '/admins/verified/mya-sg-ph-to-preparatory-program');
+
+$('#hk_selection').attr('href', env.baseUrl + '/admins/verified/hk-selection');
+$('#hk_placement').attr('href', env.baseUrl + '/admins/verified/hk-placement');
+$('#hk_to_preparatory_program').attr('href', env.baseUrl + '/admins/verified/hk-to-preparatory-program');
+$('#hk_foreign_edu_selection').attr('href', env.baseUrl + '/admins/verified/hk-foreign-edu-selection');
+$('#hk_foreign_edu_placement').attr('href', env.baseUrl + '/admins/verified/hk-foreign-edu-placement');
+
+
+$('#malaysia_spring').attr('href', env.baseUrl + '/admins/verified/malaysia-spring');
+
+
