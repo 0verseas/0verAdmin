1a287a6a8cdef343aba57800f028244381c48334
richegg <zxp86021@gmail.com>
2018-12-04 15:30:53 +0800

    新增名額表格

---
+++ src/js/estimateAdmissionCountFile.js
@@ -0,0 +1,54 @@
+var estimateAdmissionCountFile = (function () {
+    /**
+     * cache DOM
+     */
+    const $all_university = $('#all_university');
+    const $all_technology = $('#all_technology');
+    const $all_all = $('#all_all');
+
+    const $bachelor_university = $('#bachelor_university');
+    const $bachelor_technology = $('#bachelor_technology');
+    const $bachelor_all = $('#bachelor_all');
+
+    const $twoYear_university = $('#twoYear_university');
+    const $twoYear_technology = $('#twoYear_technology');
+    const $twoYear_all = $('#twoYear_all');
+
+    const $master_university = $('#master_university');
+    const $master_technology = $('#master_technology');
+    const $master_all = $('#master_all');
+
+    const $phd_university = $('#phd_university');
+    const $phd_technology = $('#phd_technology');
+    const $phd_all = $('#phd_all');
+    /**
+     * bind event
+     */
+
+    /**
+     * init
+     */
+    _setData();
+
+    function _setData() {
+        $all_university.attr('href', env.baseUrl + '/admins/estimate-admission-count-file/university/system/all');
+        $all_technology.attr('href', env.baseUrl + '/admins/estimate-admission-count-file/technology/system/all');
+        $all_all.attr('href', env.baseUrl + '/admins/estimate-admission-count-file/all/system/all');
+
+        $bachelor_university.attr('href', env.baseUrl + '/admins/estimate-admission-count-file/university/system/bachelor');
+        $bachelor_technology.attr('href', env.baseUrl + '/admins/estimate-admission-count-file/technology/system/bachelor');
+        $bachelor_all.attr('href', env.baseUrl + '/admins/estimate-admission-count-file/all/system/bachelor');
+
+        $twoYear_university.attr('href', env.baseUrl + '/admins/estimate-admission-count-file/university/system/twoYear');
+        $twoYear_technology.attr('href', env.baseUrl + '/admins/estimate-admission-count-file/technology/system/twoYear');
+        $twoYear_all.attr('href', env.baseUrl + '/admins/estimate-admission-count-file/all/system/twoYear');
+
+        $master_university.attr('href', env.baseUrl + '/admins/estimate-admission-count-file/university/system/master');
+        $master_technology.attr('href', env.baseUrl + '/admins/estimate-admission-count-file/technology/system/master');
+        $master_all.attr('href', env.baseUrl + '/admins/estimate-admission-count-file/all/system/master');
+
+        $phd_university.attr('href', env.baseUrl + '/admins/estimate-admission-count-file/university/system/phd');
+        $phd_technology.attr('href', env.baseUrl + '/admins/estimate-admission-count-file/technology/system/phd');
+        $phd_all.attr('href', env.baseUrl + '/admins/estimate-admission-count-file/all/system/phd');
+    }
+})();
