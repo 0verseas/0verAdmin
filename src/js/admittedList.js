e30a7224228556d9ba135e64d91bb8ddd39d1d77
hms5232 <hms5232@yahoo.com.tw>
2019-05-20 10:57:17 +0800

    修復錄取學生 email 清冊中個人申請排除研究所&僅研究所兩個清單死掉的問題
    不知道哪時候手殘改到

--- src/js/admittedList.js
+++ src/js/admittedList.js
@@ -22,8 +22,8 @@ var admittedList = (function () {
 
     function _setData() {
         $stage_zero.attr('href', env.baseUrl + '/admins/admit/stage_zero');
-        $stage_zero_no_graduate.attr('href', env.baseUrl + '/admit/stage_zero_no_graduate');
-        $stage_zero_only_graduate.attr('href', env.baseUrl + '/admit/stage_zero_only_graduate');
+        $stage_zero_no_graduate.attr('href', env.baseUrl + '/admins/admit/stage_zero_no_graduate');
+        $stage_zero_only_graduate.attr('href', env.baseUrl + '/admins/admit/stage_zero_only_graduate');
 
         $stage_one.attr('href', env.baseUrl + '/admins/admit/stage_one');
 
