715300c2542a7e42f590dbef5a99fe2a6bea0208
ricklai82 <ricklai82@gmail.com>
2022-01-19 13:32:43 +0800

    海聯後台導入google reCAPTCHA驗證 並去除圖形驗證碼

--- src/js/login.js
+++ src/js/login.js
@@ -1,4 +1,9 @@
 var login = (function () {
+	// 引入 reCAPTCHA 的 JS 檔案
+    var s = document.createElement('script');
+    let src = 'https://www.google.com/recaptcha/api.js?render=' + env.reCAPTCHA_site_key;
+    s.setAttribute('src', src);
+    document.body.appendChild(s);
 
     /**
      * cache DOM
@@ -9,9 +14,6 @@ var login = (function () {
     var $loginBtn = $('#btn-login');
     var $errMsg = $('#errMsg');
     var $getStatus = $('#getStatus');
-    const $identifyingCanvas = $('#identifyingCanvas')
-	const $identifyingCode = $('#identifyingCode')
-	var identifyingCode = '';
 
     /**
      * bind event
@@ -19,15 +21,11 @@ var login = (function () {
 
     $loginBtn.on('click', _login);
     $password.on('keydown', _login);
-    $identifyingCode.on('keydown', _login);
     $getStatus.on('click', _getStatus);
-    $identifyingCanvas.on('click',generateCode);
 
     /**
 	*	init
 	*/
-
-	generateCode();
 
     // 登入：
     // 200: 跳轉至 /school
@@ -39,15 +37,6 @@ var login = (function () {
 
         var username = $username.val();
         var password = $password.val();
-        
-        //確認驗證碼是否一致  不區分大小寫
-		const code = $identifyingCode.val();
-		if(code.toUpperCase() !== identifyingCode){
-			alert('驗證碼不正確');
-			generateCode();
-			return;
-		}
-		generateCode();
 
         if (!username || !password) {
             return;
@@ -55,25 +44,37 @@ var login = (function () {
 
         var loginForm = {
             username: username,
-            password: sha256(password)
+            password: sha256(password),
+			google_recaptcha_token: ''
         };
 
-        User.login(loginForm).then(function(res) {
-            if(res.ok) {
-                return res.json();
-            } else {
-                throw res.status;
-            }
-        }).then(function(json) {
-            console.log(json);
-            window.location.href = './schoolEditorInfo.html'
-        }).catch(function(err) {
-            if (err == 401) {
-                $errMsg.finish().show().text('帳號密碼錯誤。').fadeOut(1500);
-            } else if(err == 429){  // 429 Too Many Requests
-                $errMsg.finish().show().text('錯誤次數太多，請稍後再試。').fadeOut(5000);
-            }
-        })
+		grecaptcha.ready(function() {
+            grecaptcha.execute(env.reCAPTCHA_site_key, {
+              action: 'AdminLogin'
+            }).then(function(token) {
+                // token = document.getElementById('btn-login').value
+                loginForm.google_recaptcha_token=token;
+            }).then(function(){
+				User.login(loginForm).then(function(res) {
+					if(res.ok) {
+						return res.json();
+					} else {
+						throw res.status;
+					}
+				}).then(function(json) {
+					console.log(json);
+					window.location.href = './schoolEditorInfo.html'
+				}).catch(function(err) {
+					if (err == 401) {
+						$errMsg.finish().show().text('帳號密碼錯誤。').fadeOut(1500);
+					} else if(err == 429){  // 429 Too Many Requests
+						$errMsg.finish().show().text('錯誤次數太多，請稍後再試。').fadeOut(5000);
+					} else if(err == 403){  // 403 Google reCaptcha Failed
+						$errMsg.finish().show().text('Google reCaptcha Failed。').fadeOut(5000);
+					}
+				})
+			});
+        });
     }
 
     function _getStatus() {
@@ -89,88 +90,5 @@ var login = (function () {
             console.log(err);
         })
     }
-
-    //產生圖形驗證碼
-	function generateCode(){
-
-		//隨機產生數字
-		function randomNumber(min, max){
-			return Math.floor(Math.random()*(max-min)+min);  //隨機產生一個在min~max之間的整數
-		}
-	
-		//隨機顏色色碼
-		function randomColor(min, max){
-			
-			let r = randomNumber(min, max);
-			let g = randomNumber(min, max);
-			let b = randomNumber(min, max);
-	
-			return "rgb("+r+","+g+","+b+")";
-		}
-
-		//取得畫布物件屬性
-		let canvas = document.getElementById('identifyingCanvas');
-		let width = canvas.width;
-		let height = canvas.height;
-		let context = canvas.getContext('2d');
-
-		//基礎設定 設置文本基線在底部  背景顏色  方形繪製
-		context.textBaseline = 'bottom';
-		context.fillStyle = randomColor(200,240);
-		context.fillRect(0,0,width,height);
-
-		//隨機字母表   去除相似的 1 I   0 O   
-		let codeList = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
-
-		let codeString = '';
-
-		//隨機產生4個字母
-		for(let i = 0; i<4 ; i++){
-			let code = codeList[randomNumber(0,codeList.length)];
-			codeString += code;
-
-			context.fillStyle = randomColor(50,160);
-			context.font = randomNumber(25,30)+ 'px Arial';  //字體大小25~30隨機
-
-			let x = 10+i*25;
-			let y = randomNumber(30,35);  //隨機高度
-			let angle = randomNumber(-30,30);  //隨機旋轉角度
-
-			context.translate(x,y);  //移動繪製初始位置
-			context.rotate(angle*Math.PI/180);  //旋轉繪製初始位置
-
-			context.fillText(code,0,0);
-
-			context.rotate(-angle*Math.PI/180);  //返回繪製初始位置
-			context.translate(-x,-y);  //返回繪製初始位置
-		}
-
-		//產生干擾線
-		for(let i =0;i<2;i++){
-			context.strokeStyle = randomColor(40,180);
-
-			context.beginPath();
-
-			context.moveTo( randomNumber(0,width), randomNumber(0,height));
-
-			context.lineTo( randomNumber(0,width), randomNumber(0,height));
-
-			context.stroke();
-		}
-
-		//產生干擾點
-		for(let i=0 ; i<50 ; i++){
-			context.fillStyle = randomColor(0,255);
-
-			context.beginPath();
-			
-			context.arc( randomNumber(0,width), randomNumber(0,height),1,0,2*Math.PI);
-
-			context.fill();
-		}
-
-		//紀錄驗證碼
-		identifyingCode = codeString;
-	}
 
 })();
